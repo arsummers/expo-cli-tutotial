bf3c924a78ad6e4c09fc0987c0b1e8844d89440c
arsummers <aliyasummers1@gmail.com>
2020-05-14 16:37:06 -0700

    changed some colors

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Heyo this thing is onnnn</Text>
-      <Text>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text style={styles.instructions}>
         I think maybe I should start with a simple counter app, but perhaps I should start with the actual tutorial
+
+        This marks the day when VSCode decided to stop opening stuff. Think of it as a placeholder of sorts
 
         To share a photo from your phone with a friend, just press the button below!
       </Text>
@@ -17,8 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#afbcf0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#1a0136',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }, 
 });
